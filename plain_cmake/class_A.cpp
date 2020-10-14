9cb0e88ea7885bdcf6738e702d103aa257dc8a86
Giovanni <giovannipini@hopetechnik.com>
2020-10-14 09:38:34 +0800

    Adding Cmake file

---
+++ plain_cmake/class_A.cpp
@@ -0,0 +1,9 @@
+#include "class_A.h"
+
+int A::echo(int i){
+	return i;
+}
+
+int A::add(int i, int j){
+	return i+j+1;
+}
---
+++ plain_cmake/class_A.h
@@ -0,0 +1,8 @@
+class A{
+	
+	public:
+		int echo(int i);
+		
+		int add (int i, int j);
+		
+};
